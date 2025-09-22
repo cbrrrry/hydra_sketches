4782f79c4c6f25927744ba6aa9b42f4da7e58fc6
Carson Berry <carsonberry@hotmail.ca>
2025-09-21 17:51:51 -0700

    made ghost pixelate with sound, but still a little too crazy

--- sketches/backgrounds/ghost.js
+++ sketches/backgrounds/ghost.js
@@ -2,15 +2,19 @@ await s0.initImage("https://static.vecteezy.com/system/resources/previews/010/33
 
 a.show()
 a.setBins(5)
+a.setSmooth(0.2)
+a.setCutoff( cutoff = 3)
 src(s0)
-  .scale(.1, 1, 
+  .scale(.1, 1,
          () => window.innerWidth/window.innerHeight)
-  .scroll(1, 20, ()=>a.fft(0)*0.1, ()=>a.fft(3)*0.0001)
-  .brightness(.10)
-//   .saturate(4)
+  .scroll(1, 10,0.1, 0.001)
+  .brightness( ()=>a.fft[0]*1)
+  .saturate(4)
   .modulateScrollY(osc(9, .1).rotate(0, .3))
-  .add(osc(9, ()=>a.fft(0)*.01, Math.PI / 2)
-       .modulatePixelate(voronoi(), 
+  .modulatePixelate( osc( 0.2), ()=>a.fft[4]*10000)
+  .add(osc(9, .01, Math.PI / 2)
+       .modulatePixelate(voronoi(),
                          [9, 27].ease('easeInOutCubic').fast(.9))
       .rotate(0, -.3), .5)
   .out()
+
